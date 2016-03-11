457064d5ef0ff0c7a71c78dfb570269c1a5bc8fd
gj <gj@itcast.cn>
2016-03-11 16:26:22 +0800

    初始化项目

---
+++ chatQQ/chatQQ/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  chatQQ
+//
+//  Created by apple on 16/3/11.
+//  Copyright © 2016年 m14a.cn. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
